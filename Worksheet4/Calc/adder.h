ae1b403ffd5af6a6310c21d07880f5632dd9b8b4
Tanishq Arora <eeyta11@nottingham.ac.uk>
2026-02-13 10:37:36 +0000

    Worksheet 4 Exercise 4: dynamic library build with CMake

--- Worksheet4/Calc/adder.h
+++ Worksheet4/Calc/adder.h
@@ -1,6 +1,23 @@
 #ifndef ADDER_H
 #define ADDER_H
 
-int add(int a, int b);
+// If we are building/using a STATIC library, we do NOT use dllimport/dllexport
+#ifdef maths_STATIC
+  #define MATHSLIB_API
+#else
+  // Dynamic library (Windows): export from DLL or import into EXE
+  #ifdef _WIN32
+    #ifdef maths_EXPORTS
+      #define MATHSLIB_API __declspec(dllexport)
+    #else
+      #define MATHSLIB_API __declspec(dllimport)
+    #endif
+  #else
+    #define MATHSLIB_API
+  #endif
+#endif
+
+MATHSLIB_API int add(int a, int b);
 
 #endif
+
